a4360c011570bed904f13ea54c3d29bba2cf83ba
Victor mourão <victormourao2504@gmail.com>
2024-10-11 12:15:25 -0300

    11:10

--- aula04/src/App.js
+++ aula04/src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState}from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const[vetor,setvetor] = useState(["victor","antonio","estevao","mourao"]);
+  const[aluno,setaluno] = useState("")
+
+  function adicionar()
+  {
+  setvetor([...vetor,aluno]);
+  setaluno ("");
+  }
+
+
+    return (
+      <div>
+          <h1>aula 04 - estudo de vetores</h1>
+
+
+        <div className="conteudo"></div>
+          <h3>entrada de dados</h3>
+          <form>
+            <p>
+             digite o nome aluno <br />
+              <input type="text" value={aluno} onChange={(e) => setaluno(e.target.value)} />
+            </p>
+
+            <p>
+
+              <butoon type="butoon"onclick={adicionar}>adicionar o nome da lista</butoon>
+            </p>
+          </form>
+        </div>
+
+
+
+        <div className="conteudo">
+            <h3>lista de alunos</h3>
+
+            <ul>
+
+              {vetor.map(nome => (<li>{nome}</li>))}
+
+            </ul>
+
+        </div>
   );
 }
 
